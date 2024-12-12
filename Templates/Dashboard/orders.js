a9b3254149ce0f86a15a12f76553b8fc2b40a920
PablohMiranda <pablohskobar@gmail.com>
2024-12-12 17:55:08 -0300

    Add Dashboard

---
+++ Templates/Dashboard/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'One House',
+        productNumber: '85743',
+        paymentStatus: 'Pulo Augusto',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Grupo Maxi',
+        productNumber: '36452',
+        paymentStatus: 'Paula Nonato',
+        status: 'Cancelado'
+    },
+    {
+        productName: 'One House',
+        productNumber: '97245',
+        paymentStatus: 'Regiane Rocha',
+        status: 'Aprovado'
+    },
+]
